69ca08634530fd6b6bb63c776b23e179fb3ca6ea
Will-Key <saibe.akreiry@gmail.com>
2023-01-25 14:11:00 +0000

    Try to fix request on LeTexto API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  production: true,
   firebase: {
     projectId: 'megj-a6b90',
     appId: '1:326413185288:web:270c313a3cb7241647fcbd',
@@ -18,13 +19,13 @@ export const environment = {
   letextokey: 'wnScP3MdEWFYSWOgKKr9KWoLHVytFq',
   letextosender: '0575806605',
   message: `Paiement du droit de 5000FCFA par OM ou WAVE au 0787041347.
-  Par western,RIA et MoneyGram avec les références ci dessous :
-  Nom: AKE
-  Prénoms : AUDREY MANUELA
-  Possibilité de payer pour le pack Gabaon à 20.000FCFA.
-  Veuillez laisser un message sur le numéro de paiement après que celui ci soit effectif.
-  Délai de Paiement : 12/02/2023 
-  `,
+Par western,RIA et MoneyGram avec les références ci dessous :
+Nom: AKE
+Prénoms : AUDREY MANUELA
+Possibilité de payer pour le pack Gabaon à 20.000FCFA.
+Veuillez laisser un message sur le numéro de paiement après que celui ci soit effectif.
+Délai de Paiement : 12/02/2023 
+    `,
 }
 
 /*
